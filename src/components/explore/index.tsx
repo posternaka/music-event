6f60ae4c14d35ea38cefd4866d1f97e80d1013e5
posternaka <nik.petrenko97@gmail.com>
2024-05-26 11:48:33 +0300

    explore: added swiper for loop-slides

--- src/components/explore/index.tsx
+++ src/components/explore/index.tsx
@@ -4,6 +4,8 @@ import Icon3 from '@/assets/explore-card-3.png';
 import Icon4 from '@/assets/explore-card-4.png';
 import Icon5 from '@/assets/explore-card-5.png';
 import '@/components/explore/explore.css';
+import 'swiper/css';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 type Props = {};
 
@@ -14,52 +16,83 @@ const Explore = (props: Props) => {
 				<h2 className='explore__title'>
 					<span className='explore__title--color'>Explore</span> By Category
 				</h2>
-			</div>
+				<div className='explore__cards flex flex-center'>
+					<Swiper spaceBetween={20} slidesPerView={3} loop={true}>
+						<SwiperSlide>
+							<div className='explore__item explore__item--1'>
+								<img
+									src={Icon1}
+									alt='explore-icon-1'
+									className='explore__icon'
+								/>
+								<p className='item__title'>Pop</p>
+								<p className='item__description'>
+									Duis nec sodales nibh. Proin lectus tortor, rutrum vel
+									vulputate vitae
+								</p>
+							</div>
+						</SwiperSlide>
 
-			<div className='explore__cards flex flex-center'>
-				<div className='explore__item explore__item--1'>
-					<img src={Icon1} alt='explore-icon-1' className='explore__icon' />
-					<p className='item__title'>Pop</p>
-					<p className='item__description'>
-						Duis nec sodales nibh. Proin lectus tortor, rutrum vel vulputate
-						vitae
-					</p>
-				</div>
+						<SwiperSlide>
+							<div className='explore__item explore__item--2'>
+								<img
+									src={Icon2}
+									alt='explore-icon-2'
+									className='explore__icon'
+								/>
+								<p className='item__title'>Folk</p>
+								<p className='item__description'>
+									Duis nec sodales nibh. Proin lectus tortor, rutrum vel
+									vulputate vitae
+								</p>
+							</div>
+						</SwiperSlide>
 
-				<div className='explore__item explore__item--2'>
-					<img src={Icon2} alt='explore-icon-2' className='explore__icon' />
-					<p className='item__title'>Folk</p>
-					<p className='item__description'>
-						Duis nec sodales nibh. Proin lectus tortor, rutrum vel vulputate
-						vitae
-					</p>
-				</div>
+						<SwiperSlide>
+							<div className='explore__item explore__item--3'>
+								<img
+									src={Icon3}
+									alt='explore-icon-3'
+									className='explore__icon'
+								/>
+								<p className='item__title'>Instrumental</p>
+								<p className='item__description'>
+									Duis nec sodales nibh. Proin lectus tortor, rutrum vel
+									vulputate vitae
+								</p>
+							</div>
+						</SwiperSlide>
 
-				<div className='explore__item explore__item--3'>
-					<img src={Icon3} alt='explore-icon-3' className='explore__icon' />
-					<p className='item__title'>Instrumental</p>
-					<p className='item__description'>
-						Duis nec sodales nibh. Proin lectus tortor, rutrum vel vulputate
-						vitae
-					</p>
-				</div>
+						<SwiperSlide>
+							<div className='explore__item explore__item--4'>
+								<img
+									src={Icon4}
+									alt='explore-icon-4'
+									className='explore__icon'
+								/>
+								<p className='item__title'>Rock</p>
+								<p className='item__description'>
+									Duis nec sodales nibh. Proin lectus tortor, rutrum vel
+									vulputate vitae
+								</p>
+							</div>
+						</SwiperSlide>
 
-				<div className='explore__item explore__item--4'>
-					<img src={Icon4} alt='explore-icon-4' className='explore__icon' />
-					<p className='item__title'>Rock</p>
-					<p className='item__description'>
-						Duis nec sodales nibh. Proin lectus tortor, rutrum vel vulputate
-						vitae
-					</p>
-				</div>
-
-				<div className='explore__item explore__item--5'>
-					<img src={Icon5} alt='explore-icon-5' className='explore__icon' />
-					<p className='item__title'>Jazz</p>
-					<p className='item__description'>
-						Duis nec sodales nibh. Proin lectus tortor, rutrum vel vulputate
-						vitae
-					</p>
+						<SwiperSlide>
+							<div className='explore__item explore__item--5'>
+								<img
+									src={Icon5}
+									alt='explore-icon-5'
+									className='explore__icon'
+								/>
+								<p className='item__title'>Jazz</p>
+								<p className='item__description'>
+									Duis nec sodales nibh. Proin lectus tortor, rutrum vel
+									vulputate vitae
+								</p>
+							</div>
+						</SwiperSlide>
+					</Swiper>
 				</div>
 			</div>
 		</section>
